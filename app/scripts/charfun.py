2803afb8539c780bbf53cc8ed1c92d98541466a8
jdommil <jdommil@g.educaand.es>
2024-12-10 13:00:27 +0100

    Update charfun.py

--- app/scripts/charfun.py
+++ app/scripts/charfun.py
@@ -1,11 +1,14 @@
 """
 charfun.py
 
-Programa que determina si una cadena proporcionada por el usuario es palíndroma. Para ello se preguntará por teclado al usuario tantas veces como quiera hasta que escriba la palabra salir.
+Programa que determina si una cadena proporcionada por el
+usuario es palíndroma. Para ello se preguntará por teclado al
+usuario tantas veces como quiera hasta que escriba la palabra
+salir.
 
-Última Modificación: 29/11/2024
-Autor: Jaime Domínguez Millón
-Dependencias: Unicodedata
+Última Modificación: 21/11/2024
+Autor: Jaime Domín Domínguez Millón
+Dependencias: unicodedata
 """
 
 import unicodedata
@@ -15,19 +18,27 @@ def esPalindromo(cadena):
     Función que verifica si una cadena es palíndroma.
     Ignora espacios, mayúsculas y tildes.
     """
-    # Convertir la cadena a minúsculas y eliminar caracteres no alfabéticos
-    cadena_limpia = ''.join(char.lower() for char in cadena if char.isalnum())
-    
+    # Convertir a minúsculas
+    cadena = cadena.lower()
+    # Eliminar acentos y diacríticos
+    cadena = ''.join(
+        char for char in unicodedata.normalize('NFD', cadena)
+        if unicodedata.category(char) != 'Mn'
+    )
+    # Eliminar caracteres no alfanuméricos
+    cadena_limpia = ''.join(char for char in cadena if char.isalnum())
     # Comparar la cadena limpia con su reverso
     return cadena_limpia == cadena_limpia[::-1]
 
-frase = input("Introduce una frase (o escribe 'salir' para terminar): ")
-
-if frase.lower() == "salir":
-    print("Programa finalizado.")
-else:
-    # Comprobar si es palíndromo
-    if esPalindromo(frase):
-        print("La frase es un palíndromo.")
-    else:
-        print("La frase no es un palíndromo.")
+if __name__ == '__main__':
+    while True:
+        frase = input("Introduce una frase (o escribe 'salir' para terminar): ")
+        if frase.lower() == "salir":
+            print("Programa finalizado.")
+            break
+        else:
+            # Comprobar si es palíndromo
+            if esPalindromo(frase):
+                print("La frase es palíndroma.")
+            else:
+                print("La frase no es palíndroma.")
